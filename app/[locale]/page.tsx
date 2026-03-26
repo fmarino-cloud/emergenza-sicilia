c33639f1391079d592355d2d6c15705a67d11b9b
fmarino-cloud <fmarino@gds.it>
2026-03-26 12:11:26 +0100

    Update page.tsx

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -73,7 +73,6 @@ export default async function HomePage() {
     prisma.sourceItem.findMany({
       distinct: ["source"],
       select: { source: true },
-      orderBy: { createdAt: "desc" },
     }),
   ]);
 
